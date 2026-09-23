907b344a03e17df6b0fcb1d57a05e247d4bd39cd
T3Gold <mainguyentruongduy60539@gmail.com>
2026-09-23 11:12:26 +0700

    Giu nguyen 2 so thap phan nguyen ban cho Vang TG khong lam tron theo dung thap phan bieu do va VangSaigon

--- public/app.js
+++ public/app.js
@@ -725,8 +725,8 @@ const GoldDashboard = (function () {
                 if (item.isWorld) {
                     const buyNum = Number(item.buy) || 0;
                     const sellNum = Number(item.sell) || 0;
-                    buyMain = `${Math.floor(buyNum).toLocaleString('en-US')}`;
-                    sellMain = `${Math.floor(sellNum).toLocaleString('en-US')}`;
+                    buyMain = buyNum.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                    sellMain = sellNum.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
                     
                     let vndPerChiSell = 0;
                     let vndPerChiBuy = 0;
